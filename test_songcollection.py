e9c7339b5a23ab4e08bdc353f1ab4ea5f4cddaac
LINYE <linye1052@gmail.com>
2023-11-23 00:39:52 +1000

    SongCollection and its test

--- test_songcollection.py
+++ test_songcollection.py
@@ -1,7 +1,7 @@
-"""(Incomplete) Tests for SongCollection class."""
+"""Tests for SongCollection class"""
+
 from song import Song
 from songcollection import SongCollection
-
 
 def run_tests():
     """Test SongCollection class."""
@@ -27,11 +27,41 @@ def run_tests():
     print("Test sorting - year:")
     song_collection.sort("year")
     print(song_collection)
-    # TODO: Add more sorting tests
 
-    # TODO: Test saving songs (check file manually to see results)
+    # 2DO: Add more sorting tests
 
-    # TODO: Add more tests, as appropriate, for each method
+    # Test sorting songs by artist
+    print("Test sorting - artist:")
+    song_collection.sort("artist")
+    print(song_collection)
+
+    # Test sorting songs by title
+    print("Test sorting - title:")
+    song_collection.sort("title")
+    print(song_collection)
+
+    # Test marking a song as unlearned
+    print("Test marking a song as unlearned:")
+    song_collection.songs[0].mark_as_unlearned()
+    print(song_collection)
+
+    # 2DO: Test saving songs (check file manually to see results)
+
+    # Test saving songs
+    print("Test saving songs:")
+    song_collection.save_songs('songs.json')
+
+    # 2DO: Add more tests, as appropriate, for each method
+
+    # Test finding songs by artist
+    print("Test finding songs by artist:")
+    artist_search_result = song_collection.find_songs_by_artist("Powderfinger")
+    print(artist_search_result)
+
+    # Test finding songs by year
+    print("Test finding songs by year:")
+    year_search_result = song_collection.find_songs_by_year(1996)
+    print(year_search_result)
 
 
 run_tests()
