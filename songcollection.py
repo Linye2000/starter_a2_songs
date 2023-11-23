57ff5af2406b8a6b50c8141658fdb60a39aa1ce5
LINYE <linye1052@gmail.com>
2023-11-23 15:38:45 +1000

    SongCollection Class and its test

--- songcollection.py
+++ songcollection.py
@@ -1,46 +1,48 @@
 """SongCollection Class"""
 
-# 2DO: Create your SongCollection class in this file
-
 import json
 from song import Song
 
 class SongCollection:
+    # Constructor method to initialize a new SongCollection object
     def __init__(self):
-        """Constructor for the SongCollection class."""
-        self.songs = []
+        self.songs = []  # Initialize an empty list to store Song objects
 
+    # Method to add a song to the collection
     def add_song(self, song):
-        """Add a single Song object to the song list."""
-        self.songs.append(song)
+        self.songs.append(song)  # Append the song to the songs list
 
+    # Method to calculate the number of unlearned songs in the collection
     def get_number_of_unlearned_songs(self):
-        """Get the number of unlearned songs."""
+        # Returns the count of songs that are not marked as learned
         return sum(not song.is_learned for song in self.songs)
 
+    # Method to calculate the number of learned songs in the collection
     def get_number_of_learned_songs(self):
-        """Get the number of learned songs."""
+        # Returns the count of songs that are marked as learned
         return sum(song.is_learned for song in self.songs)
 
+    # Method to load songs from a JSON file
     def load_songs(self, filename):
-        """Load songs from a JSON file into the list of Song objects."""
-        with open(filename, 'r') as file:
-            songs_data = json.load(file)
-            for song_data in songs_data:
+        with open(filename, 'r') as file:  # Open the file for reading
+            songs_data = json.load(file)  # Load the JSON data from the file
+            for song_data in songs_data:  # Iterate over each song in the JSON data
+                # Create a Song object and add it to the list
                 song = Song(song_data['title'], song_data['artist'], song_data['year'], song_data['is_learned'])
                 self.songs.append(song)
 
+    # Method to save the songs to a JSON file
     def save_songs(self, filename):
-        """Save songs from the song list into a JSON file."""
-        songs_data = [song.to_dict() for song in self.songs]  # Convert each song to a dictionary.
-        with open(filename, 'w') as file:
-            json.dump(songs_data, file, indent=2)  # Write the song data to the file in JSON format.
+        songs_data = [song.to_dict() for song in self.songs]  # Convert each song to a dictionary
+        with open(filename, 'w') as file:  # Open the file for writing
+            json.dump(songs_data, file, indent=2)  # Write the song data to the file in JSON format
 
-
+    # Method to sort the songs in the collection
     def sort(self, key):
-        """Sort songs by the key passed in, then by title."""
+        # Sort the songs based on a specified attribute, defaulting to title if the attribute is the same
         self.songs.sort(key=lambda song: (getattr(song, key), song.title))
 
+    # Magic method to define the string representation of a SongCollection object
     def __str__(self):
-        """String representation of the SongCollection object."""
+        # Joining each song's string representation with a newline
         return "\n".join(str(song) for song in self.songs)
--- test_songcollection.py
+++ test_songcollection.py
@@ -4,64 +4,61 @@ from song import Song
 from songcollection import SongCollection
 
 def run_tests():
-    """Test SongCollection class."""
-
-    # Test empty SongCollection (defaults)
+    # Test case for an empty SongCollection
     print("Test empty SongCollection:")
-    song_collection = SongCollection()
-    print(song_collection)
-    assert not song_collection.songs  # an empty list is considered False
+    song_collection = SongCollection()  # Creating an empty SongCollection object
+    print(song_collection)  # Displaying the empty collection
+    assert not song_collection.songs  # Assert that the songs list is empty
 
-    # Test loading songs
+    # Test case for loading songs from a file
     print("Test loading songs:")
-    song_collection.load_songs('songs.json')
-    print(song_collection)
-    assert song_collection.songs  # assuming file is non-empty, non-empty list is considered True
+    song_collection.load_songs('songs.json')  # Loading songs from a JSON file
+    print(song_collection)  # Displaying the collection after loading songs
+    assert song_collection.songs  # Assert that the collection is not empty after loading songs
 
-    # Test adding a new Song with values
+    # Test case for adding a new song to the collection
     print("Test adding new song:")
-    song_collection.add_song(Song("My Happiness", "Powderfinger", 1996, True))
-    print(song_collection)
+    song_collection.add_song(Song("My Happiness", "Powderfinger", 1996, True))  # Adding a new song
+    print(song_collection)  # Displaying the collection after adding a song
 
-    # Test sorting songs
+    # Test cases for sorting the songs in different ways
     print("Test sorting - year:")
-    song_collection.sort("year")
-    print(song_collection)
+    song_collection.sort("year")  # Sorting songs by year
+    print(song_collection)  # Displaying the collection after sorting by year
 
     # 2DO: Add more sorting tests
 
     # Test sorting songs by artist
     print("Test sorting - artist:")
-    song_collection.sort("artist")
-    print(song_collection)
+    song_collection.sort("artist")  # Sorting songs by artist
+    print(song_collection)  # Displaying the collection after sorting by artist
 
     # Test sorting songs by title
     print("Test sorting - title:")
-    song_collection.sort("title")
-    print(song_collection)
+    song_collection.sort("title")  # Sorting songs by title
+    print(song_collection)  # Displaying the collection after sorting by title
 
-    # Test marking a song as unlearned
+    # Test case for marking a song as unlearned
     print("Test marking a song as unlearned:")
-    song_collection.songs[0].mark_as_unlearned()
-    print(song_collection)
+    song_collection.songs[0].mark_as_unlearned()  # Marking the first song as unlearned
+    print(song_collection)  # Displaying the collection after marking a song as unlearned
 
     # 2DO: Test saving songs (check file manually to see results)
 
-    # Test saving songs
+    # Test case for saving songs to a file
     print("Test saving songs:")
-    song_collection.save_songs('songs.json')
+    song_collection.save_songs('songs.json')  # Saving the current state of the collection to a file
 
     # 2DO: Add more tests, as appropriate, for each method
 
-    # Test finding songs by artist
+    # Test cases for finding songs by a specific artist or year
     print("Test finding songs by artist:")
-    artist_search_result = song_collection.find_songs_by_artist("Powderfinger")
-    print(artist_search_result)
+    artist_search_result = song_collection.find_songs_by_artist("Powderfinger")  # Finding songs by the artist "Powderfinger"
+    print(artist_search_result)  # Displaying the result of the artist search
 
     # Test finding songs by year
     print("Test finding songs by year:")
-    year_search_result = song_collection.find_songs_by_year(1996)
-    print(year_search_result)
-
+    year_search_result = song_collection.find_songs_by_year(1996)  # Finding songs from the year 1996
+    print(year_search_result)  # Displaying the result of the year search
 
 run_tests()
